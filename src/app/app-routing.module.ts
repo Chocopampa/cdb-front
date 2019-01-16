45b5eae4075495ad35f38048d173b9d859041fec
Chocopampa <jerome.guenet@reseau.eseo.fr>
2019-01-16 17:23:24 +0100

    commit part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CompanyListComponent } from './company/routed/company-list/company-list.component';
+import { CompanyOverviewComponent } from './company/routed/company-overview/company-overview.component';
 
 const routes: Routes = [
   {
     path: 'companies',
     component: CompanyListComponent
-  }, {
+  },
+  {
+    path: 'companies/:id',
+    component: CompanyOverviewComponent
+  },
+  {
     path: '**',
     redirectTo: 'companies'
   }
