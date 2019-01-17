c8bda279fca3236edb635375e3fddf408154db9b
Chocopampa <jerome.guenet@reseau.eseo.fr>
2019-01-17 14:11:31 +0100

    Creation of company functional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CompanyListComponent } from './company/routed/company-list/company-list.component';
 import { CompanyOverviewComponent } from './company/routed/company-overview/company-overview.component';
+import { CompanyCreateComponent } from './company/routed/company-create/company-create.component';
 
 const routes: Routes = [
   {
     path: 'companies',
     component: CompanyListComponent
+  },
+  {
+    path: 'companies/create',
+    component: CompanyCreateComponent
   },
   {
     path: 'companies/:id',
