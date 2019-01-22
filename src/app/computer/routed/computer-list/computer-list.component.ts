2e27517a94b218639f8a801fa2acc20a3b4be3ea
Chocopampa <jerome.guenet@reseau.eseo.fr>
2019-01-22 18:07:52 +0100

    [FEATURE]Computer listing functional with css

--- src/app/computer/routed/computer-list/computer-list.component.ts
+++ src/app/computer/routed/computer-list/computer-list.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Computer } from '../../shared/computer.model';
 import { ComputerService } from '../../shared/computer.service';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-computer-list',
@@ -8,19 +12,112 @@ import { ComputerService } from '../../shared/computer.service';
   styleUrls: ['./computer-list.component.scss']
 })
 export class ComputerListComponent implements OnInit {
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+
+  pageEvent: PageEvent;
+  pageSizeOptions: number[] = [10, 50, 100];
+
+  columnsNames: string[] = ['select', 'name', 'introduced', 'discontinued', 'company_name', 'suppress'];
 
   computers: Computer[];
+  dataSource: MatTableDataSource<Computer>;
+  selection: SelectionModel<Computer>;
+
+  order_by: string;
+  type_ascend = 'ASC';
+  search: string;
+  limit: string;
+  offset: string;
+
+  nb_computers: number;
 
   constructor(
-    private _computerService: ComputerService
-    ) { }
+    private _computerService: ComputerService,
+    private _route: ActivatedRoute
+  ) {
+    this.order_by = this._route.snapshot.queryParamMap.get('order');
+    this.type_ascend = this._route.snapshot.queryParamMap.get('type');
+    this.search = this._route.snapshot.queryParamMap.get('search');
+    this.limit = this._route.snapshot.queryParamMap.get('limit');
+    this.offset = this._route.snapshot.queryParamMap.get('offset');
+  }
 
   ngOnInit() {
-    this._computerService.getAllComputers().subscribe(computers => this.computers = computers);
+    this.loadComputerList();
+    this._computerService.getComputerCount().subscribe(nb => {
+      this.nb_computers = nb;
+    });
+    this.selection = new SelectionModel<Computer>(true, []);
+  }
+
+  loadComputerList() {
+    this._computerService
+      .getComputersSpecified(
+        this.order_by,
+        this.type_ascend,
+        this.search,
+        this.limit,
+        this.offset
+      )
+      .subscribe(computers => {
+        this.computers = computers;
+        this.dataSource = new MatTableDataSource<Computer>(this.computers);
+        this.selection.clear();
+        this._computerService.getComputerCount().subscribe(nb => {
+          this.nb_computers = nb;
+        });
+      });
   }
 
   suppress(computer: Computer) {
-    this.computers.splice(this.computers.indexOf(computer), 1);
+    for (const c of this.selection.selected) {
+      this.computers.splice(this.computers.indexOf(c), 1);
+      this._computerService
+        .deleteComputer(c.id)
+        .subscribe(() => this.loadComputerList());
+    }
   }
 
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  orderBy(order: string) {
+    this.triggerAscendingType();
+    this.order_by = order;
+    this.loadComputerList();
+  }
+
+  triggerAscendingType() {
+    if (this.type_ascend === 'ASC') {
+      this.type_ascend = 'DESC';
+    } else {
+      this.type_ascend = 'ASC';
+    }
+  }
+
+  changePage() {
+    let l = +this.limit;
+    let o = +this.offset;
+    if (this.pageEvent.pageIndex) {
+      l = o * this.paginator._pageIndex;
+      this.limit = l.toString();
+    } else {
+      if (this.pageEvent.pageSize) {
+        o = this.paginator.pageSize;
+        this.limit = '0';
+        this.offset = o.toString();
+      }
+    }
+    this.loadComputerList();
+  }
 }
--- src/app/computer/shared/computer.service.ts
+++ src/app/computer/shared/computer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Computer } from './computer.model';
 import { Observable } from 'rxjs';
 
@@ -14,16 +14,43 @@ export class ComputerService {
     return this._httpClient.get<Computer[]>(`${this.SERVER_URL}`);
   }
 
+  getComputersSpecified(
+    order_by: string,
+    type_ascend: string,
+    search: string,
+    limit: string,
+    offset: string
+  ): Observable<Computer[]> {
+    return this._httpClient.get<Computer[]>(`${this.SERVER_URL}`, {
+      params: new HttpParams()
+        .set('order', order_by)
+        .set('type', type_ascend)
+        .set('search', search)
+        .set('limit', limit)
+        .set('offset', offset)
+    });
+  }
+
+  getComputerCount(): Observable<number> {
+    return this._httpClient.get<number>(`${this.SERVER_URL}/count`);
+  }
+
   getComputer(id: string): Observable<Computer> {
     return this._httpClient.get<Computer>(`${this.SERVER_URL}/${id}`);
   }
 
   createComputer(computer: Computer): Observable<Computer> {
-    return this._httpClient.post<Computer>(`${this.SERVER_URL}/create`, computer);
+    return this._httpClient.post<Computer>(
+      `${this.SERVER_URL}/create`,
+      computer
+    );
   }
 
   updateComputer(computer: Computer): Observable<Computer> {
-    return this._httpClient.put<Computer>(`${this.SERVER_URL}/update/${computer.id}`, computer);
+    return this._httpClient.put<Computer>(
+      `${this.SERVER_URL}/update/${computer.id}`,
+      computer
+    );
   }
 
   deleteComputer(id: number): Observable<Computer> {
