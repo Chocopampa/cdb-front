c3eef9da3c78c4aa3cbed2a0e391335f1aa6b1ef
LorenzoSimeoni <simeoni.lorenzo06@gmail.com>
2019-01-23 17:24:16 +0100

    [DEBUG] delete reference to delete files

--- src/app/computer/computer.module.ts
+++ src/app/computer/computer.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComputerListComponent } from './routed/computer-list/computer-list.component';
-import { ComputerDetailComponent } from './routed/computer-detail/computer-detail.component';
 import { CustomMaterialModule } from '../custom-material/custom-material.module';
-import { ComputerOverviewComponent } from './routed/computer-overview/computer-overview.component';
 import { ComputerUpdateComponent } from './routed/computer-update/computer-update.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -12,8 +10,6 @@ import { ComputerCreateComponent } from './routed/computer-create/computer-creat
 @NgModule({
   declarations: [
     ComputerListComponent,
-    ComputerDetailComponent,
-    ComputerOverviewComponent,
     ComputerUpdateComponent, ComputerCreateComponent
   ],
   imports: [
